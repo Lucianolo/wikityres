7f0cefcbb459b522bf12e0796effd6a0f7b459e9
Luciano Ursache <lucisno.ursache@gmail.com>
2017-01-25 15:17:34 +0000

    web concurrency 2

--- config/whacamole.rb
+++
@@ -1,9 +0,0 @@
-
-
-# you can specify which dynos to watch for each app (default: `web`):
-Whacamole.configure("wikityres") do |config|
-  config.api_token = "5681181a-1f63-4619-b3fd-832be797e7ca" # you could also paste your token in here as a string
-  config.dynos = %w{web worker}
-  config.restart_threshold = 600 # in megabytes. default is 1000 (good for 2X dynos)
-  config.restart_window = 30*60 # restart rate limit in seconds. default is 30 mins.
-end
