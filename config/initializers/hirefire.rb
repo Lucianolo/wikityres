cc242a1b8b39721e01d597a75f1a4e4d363136ab
Luciano Ursache <lucisno.ursache@gmail.com>
2017-03-21 18:42:45 +0000

    Trying hirefire opensource

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,5 +1,18 @@
-HireFire::Resource.configure do |config|
-  config.dyno(:worker) do
-    HireFire::Macro::Delayed::Job.queue(mapper: :active_record)
-  end
+#HireFire::Resource.configure do |config|
+#  config.dyno(:worker) do
+#    HireFire::Macro::Delayed::Job.queue(mapper: :active_record)
+#  end
+#end
+
+HireFire.configure do |config|
+  config.environment      = nil # default in production is :heroku. default in development is :noop
+  config.max_workers      = 4   # default is 1
+  config.min_workers      = 0   # default is 0
+  config.job_worker_ratio = [
+      { :jobs => 1,   :workers => 1 },
+      { :jobs => 2,  :workers => 2 },
+      { :jobs => 3,  :workers => 3 },
+      { :jobs => 4,  :workers => 4 }
+      
+    ]
 end
